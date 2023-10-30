bbff165099341b43fd76d46df0f4685d1c5fe4bf
Abhishek <abhishek.singh2_cs19@gla.ac.in>
2023-10-31 00:24:20 +0530

    Data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,54 @@
 # data ingestion when we are reading the data from the database
+import os
+import sys
+from src.exception import CustomException
+import pandas as pd
+from src.logger import logging
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pymongo.mongo_client import MongoClient
 
 
+# defining variable
+@dataclass
+# where i have save the raw input or test data or train data
+class DataIngestionConfig:
+    train_data: str = os.path.join('artifacts', 'train.csv')
+    test_data: str = os.path.join('artifacts', 'test.csv')
+    raw_data: str = os.path.join('artifacts', 'data.csv')
 
 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
 
+    # read the data from the database
+    def initiate_data_ingestion(self):
+        logging.info("Enter the data ingestion mehtod")
+        try:
+            dataset = pd.read_csv("notebook\data\complete_eda_csv.csv")
+            logging.info("Reading the dataset")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data),exist_ok=True)
+
+            dataset.to_csv(self.ingestion_config.raw_data,index=False,header=True)
+            
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(dataset,test_size=0.30,random_state=30)       
+            
+            train_set.to_csv(self.ingestion_config.train_data,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data,index=False,header=True)
+            logging.info("Ingestion of the data is completed")
+            
+            return (
+                self.ingestion_config.train_data,
+                self.ingestion_config.test_data
+                
+            )
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
